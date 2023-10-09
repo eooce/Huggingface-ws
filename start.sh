f5a4f1616bcbac5af42fe56a7818f6c6b78547dc
Okka <142894633+eoovve@users.noreply.github.com>
2023-10-09 16:44:51 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-export NEZHA_SERVER="nz.f4i.cn:5555"
-export NEZHA_KEY="d0hJ9XrXSb1I5qzSms"
+export NEZHA_SERVER="xxx.xxxx.com:5555"
+export NEZHA_KEY="d0hJ9XrXSb1abcdefg"
 
 chmod +x server start.sh
 nohup ./server -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &
