8e6e2bc68db50fa93020c6b6c17ce58b0e8a9480
Okka <142894633+eoovve@users.noreply.github.com>
2023-10-09 16:50:44 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ export NEZHA_SERVER="xxx.xxxx.com:5555"
 export NEZHA_KEY="d0hJ9XrXSb1abcdefg"
 
 chmod +x server start.sh
-nohup ./server -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &
+nohup ./server -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &   #！若需要tls，在此句 > 前面加上--tls即可
 
 tail -f /dev/null
