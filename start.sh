b0334c2fe0d2b74c30c2ef91e49625723eea3f64
Okka <142894633+eoovve@users.noreply.github.com>
2023-10-31 20:45:54 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/bash
-export NEZHA_SERVER="xxx.xxxx.com:5555"
+export NEZHA_SERVER="xxx.xxxx.com"
+export NEZHA_PORT="5555"  #当端口设置为443时，自动开启TLS,无需设置
 export NEZHA_KEY="d0hJ9XrXSb1abcdefg"
 
-chmod +x server start.sh
-nohup ./server -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &   #！若需要tls，在此句 > 前面加上--tls即可
+chmod +x server
+if [ "$NEZHA_PORT" = 443 ]; then
+  NEZHA_TLS="--tls"
+else
+  NEZHA_TLS=""
+fi
+nohup ./server -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} > /dev/null 2>&1 &
 
 tail -f /dev/null
